c96c2ccf7e96bba150a048cf5bd86e259d8382ed
Rajak376 <112695461+Rajak376@users.noreply.github.com>
2022-09-05 16:29:14 +0530

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -5,3 +5,17 @@ if(number==number[::-1]):
       print("The number is a palindrome")
 else:
       print("Not a palindrome")
+# onther methode
+
+a=101
+res=0
+t=a
+while t!=0:
+      rem=t%10
+      res=res*10+rem
+      t=t//10
+      
+if res==a:
+      print('palandrom')
+else:
+      print('not palandrom')
