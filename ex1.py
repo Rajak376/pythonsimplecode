05d9ba8e08957abd20a26feb33680656795a9f40
Rajak376 <112695461+Rajak376@users.noreply.github.com>
2022-09-06 10:22:41 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,19 +1,43 @@
-class stu():
-    def __init__(self,name1,age):
-        self.name1=name1
-        self.age=age
 
-class stu1():
-    def __init__(self,name2,age):
-        self.name2=name2
-        self.age=age
+# Python program to demonstrate
+# multilevel inheritance
+ 
+# Base class
+ 
+ 
+class stu:
+ 
+    def __init__(self, name1):
+        self.name1 = name1
+ 
+# Intermediate class
+ 
+ 
+class stu1(stu):
+    def __init__(self, name1, name2):
+        self.name2 = name2
+ 
 
-class stu2(stu,stu1):
-    def __init__(self,name3,age,id1):
-        self.name3=name3
-        self.age=age
-        self.id1=id1
+        name1.__init__(self, name1)
+ 
+# Derived class
+ 
+ 
+class stu3(stu1):
+    def __init__(self, name1, name2, name3):
+        self.name3 = name3
+ 
+        stu1.__init__(self, name2, name1)
+ 
+    def print_name(self):
+        print("name1 :", self.name1)
+        print("name2 :", self.name2)
+        print("name3 :", self.name3)
+ 
+ 
+#  Driver code
+s1 = stu3('Prince', 'Rampal', 'Lal mani')
+print(s1.name3)
+print(s1.name2)
 
-p1=stu2('Razzak',25,1314)
-print(p1.name3)
-print(p1.age)
+
